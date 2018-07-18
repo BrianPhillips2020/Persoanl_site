e2b4a33a36c80f68ee277b177cd7cca57ac42574
BrianPhillips2020 <bphillip@udel.edu>
2018-07-18 15:38:39 -0400

    add nonfunctional navnbar and auto-resize methods

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Home.css';
 
-export class Home extends Component{
-    render(){
-        return(
-            <div>
-                <h1>Brian Phillips</h1>
+let resizeTimer;
+
+export class Home extends Component {
+    constructor(props){
+        super(props);
+        this.rootRef = React.createRef();
+        this.width = window.innerWidth;
+        this.height = window.innerHeight;
+    }
+
+
+    render() {
+        return (
+            <div style={{height: '100vh'}}>
+                <div className='navbar'>
+                    <div style={{padding: '10px'}}>Home</div>
+                    <div style={{padding: '10px'}}>Contact</div>
+                    <div style={{padding: '10px'}}>Resume</div>
+                </div>
+                <div>
+                    <h1 className='title'>Brian Phillips</h1>
+                </div>
             </div>
         )
     }
 
+    updateDimensions() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(() => {
+            this.width = window.innerWidth;
+            this.height = window.innerHeight * .9875;
+        }, 100);
+    }
 
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions.bind(this))
+    }
 }
+    
+
+
+
