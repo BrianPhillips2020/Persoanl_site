fd2e0d7e83eacbe6b695a3307f34da70775c8efc
BrianPhillips2020 <bphillip@udel.edu>
2018-07-18 14:23:39 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component, Route } from 'react';
+
+import './App.css';
+import { Home } from './Home/Home.js';
+
+class App extends Component {
+
+	render() {
+		return (
+			<div className='App'>
+				<Home />
+			</div>
+		)
+	}
+}
+
+export default App;
---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+export class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Brian Phillips</h1>
+            </div>
+        )
+    }
+
+
+}
